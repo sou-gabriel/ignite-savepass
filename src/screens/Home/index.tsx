25cf266090749ee559a7fc6c104564b94118b830
Gabriel Ramos Nogueira <dev.gabrielramos@gmail.com>
2022-04-02 15:29:02 -0300

    feat: update search text value

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -38,7 +38,7 @@ export function Home() {
   }
 
   function handleChangeInputText(text: string) {
-    // Update searchText value
+    setSearchText(text)
   }
 
   useFocusEffect(useCallback(() => {
